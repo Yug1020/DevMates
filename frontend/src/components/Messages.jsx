01afb98d0e43872eaccc71f5ab0a606c954305c8
Yug1020 <yugandhardhore@gmail.com>
2026-09-02 11:32:26 +0530

    change text msg

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Messages() {
     return(
         <div className="flex flex-row items-center justify-center min-h-screen bg-[#0a0e13] text-[#dde4dd]">
-            <h1 className="text-center text-[48px]">Messages</h1>
+            <h1 className="text-center text-[48px]">Chat Box is Under development</h1>
         </div>
     )
 }
